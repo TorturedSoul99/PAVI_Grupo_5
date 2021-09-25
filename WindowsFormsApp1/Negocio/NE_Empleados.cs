f6706d52d11f9bffa251dd521961761f3b56b060
tomasparis10 <84345749+tomasparis10@users.noreply.github.com>
2021-09-25 18:45:54 -0300

    modifique los abm empleado sucursal y raza  18:45

---
+++ WindowsFormsApp1/Negocio/NE_Empleados.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsFormsApp1.Back_end;
+using System.Data;
+
+namespace WindowsFormsApp1.Negocio
+{
+    class NE_Empleados
+    {
+        BE_Acceso_datos _BD = new BE_Acceso_datos();
+
+        public DataTable BuscarEmpleadoNombre(string nombre)
+        {
+            string sql = "SELECT * FROM empleados WHERE Nombre like '%" + nombre + "%'";
+            return _BD.EjecutarSelect(sql);
+        }
+        
+        public DataTable BuscarEmpleadoID(int num)
+        {
+            string sql = "SELECT * FROM empleados WHERE ID_empleado = " + num;
+            return _BD.EjecutarSelect(sql);
+        }
+        
+        public DataTable BuscarEmpleadoIDSucursal(int numSucursal)
+        {
+            string sql = "SELECT * FROM empleados WHERE ID_sucursal = " + numSucursal;
+            return _BD.EjecutarSelect(sql);
+        }
+        
+        public DataTable BuscarTodosEmpleados()
+        {
+            string sql = "SELECT * FROM empleados";
+            return _BD.EjecutarSelect(sql);
+        }
+    }
+}
---
+++ WindowsFormsApp1/Negocio/NE_Raza.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsFormsApp1.Back_end;
+using System.Data;
+
+namespace WindowsFormsApp1.Negocio
+{
+    class NE_Raza
+    {
+        BE_Acceso_datos _BD = new BE_Acceso_datos();
+
+        public DataTable BuscarRazaDenominacion(string nombre)
+        {
+            string sql = "SELECT * FROM raza WHERE Denominacion like '%" + nombre + "%'";
+            return _BD.EjecutarSelect(sql);
+        }
+
+        public DataTable BuscarTodosRaza()
+        {
+            string sql = "SELECT * FROM raza";
+            return _BD.EjecutarSelect(sql);
+        }
+    }
+}
