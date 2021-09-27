86ac2ccf654909ff0d1836ce08b6420649d3d2e6
Fidel <fidelcesar_@hotmail.com>
2021-09-27 00:48:54 -0300

    Cambios en el menu de medicamentos 00.48

---
+++ WindowsFormsApp1/Negocio/NE_Medicamento.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsFormsApp1.Back_end;
+using System.Data;
+
+namespace WindowsFormsApp1.Negocio
+{
+    class NE_Medicamento
+    {
+        BE_Acceso_datos _BD = new BE_Acceso_datos();
+
+        public DataTable BuscarMedicamento(string nombre)
+        {
+            string sql = "SELECT * FROM medicamentos WHERE Nombre like '%" + nombre + "%'";
+            return _BD.EjecutarSelect(sql);
+        }
+    }
+}
