3fb7ab725c5c2f10dcc12117777c0a4dfa848f27
tomasparis10 <84345749+tomasparis10@users.noreply.github.com>
2021-10-07 19:26:15 -0300

    Intentando agregar unas cosas 19:26

---
+++ WindowsFormsApp1/Formularios/ABM InformacionCanes/ABM Peso_x_Altura_x_Raza/Peso_x_altura_x_Raza.Designer.cs
@@ -0,0 +1,159 @@
+﻿
+namespace WindowsFormsApp1.Formularios.ABM_InformacionCanes
+{
+    partial class Peso_x_altura_x_Raza
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.chk_todos = new System.Windows.Forms.CheckBox();
+            this.btnConsultar = new System.Windows.Forms.Button();
+            this.txt_denominacion = new System.Windows.Forms.TextBox();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.grid_raza = new System.Windows.Forms.DataGridView();
+            this.Nuevo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Consultar = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Modificar = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnNuevo = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.grid_raza)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chk_todos
+            // 
+            this.chk_todos.AutoSize = true;
+            this.chk_todos.Location = new System.Drawing.Point(133, 42);
+            this.chk_todos.Name = "chk_todos";
+            this.chk_todos.Size = new System.Drawing.Size(56, 17);
+            this.chk_todos.TabIndex = 29;
+            this.chk_todos.Text = "Todos";
+            this.chk_todos.UseVisualStyleBackColor = true;
+            // 
+            // btnConsultar
+            // 
+            this.btnConsultar.Location = new System.Drawing.Point(6, 8);
+            this.btnConsultar.Name = "btnConsultar";
+            this.btnConsultar.Size = new System.Drawing.Size(90, 23);
+            this.btnConsultar.TabIndex = 28;
+            this.btnConsultar.Text = "Consultar";
+            this.btnConsultar.UseVisualStyleBackColor = true;
+            // 
+            // txt_denominacion
+            // 
+            this.txt_denominacion.Location = new System.Drawing.Point(133, 11);
+            this.txt_denominacion.Name = "txt_denominacion";
+            this.txt_denominacion.Size = new System.Drawing.Size(276, 20);
+            this.txt_denominacion.TabIndex = 27;
+            this.txt_denominacion.Text = "Buscar Segun denominacion...";
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Location = new System.Drawing.Point(196, 312);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(119, 23);
+            this.btnModificar.TabIndex = 26;
+            this.btnModificar.Text = "Modificar";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            // 
+            // grid_raza
+            // 
+            this.grid_raza.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grid_raza.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Nuevo,
+            this.Consultar,
+            this.Modificar});
+            this.grid_raza.Location = new System.Drawing.Point(75, 89);
+            this.grid_raza.Name = "grid_raza";
+            this.grid_raza.Size = new System.Drawing.Size(372, 179);
+            this.grid_raza.TabIndex = 25;
+            // 
+            // Nuevo
+            // 
+            this.Nuevo.HeaderText = "Column1";
+            this.Nuevo.Name = "Nuevo";
+            // 
+            // Consultar
+            // 
+            this.Consultar.HeaderText = "Column1";
+            this.Consultar.Name = "Consultar";
+            // 
+            // Modificar
+            // 
+            this.Modificar.HeaderText = "Column1";
+            this.Modificar.Name = "Modificar";
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(344, 312);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(103, 23);
+            this.btnEliminar.TabIndex = 24;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.Location = new System.Drawing.Point(75, 312);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(90, 23);
+            this.btnNuevo.TabIndex = 23;
+            this.btnNuevo.Text = "Nueva";
+            this.btnNuevo.UseVisualStyleBackColor = true;
+            // 
+            // Peso_x_altura_x_Raza
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(474, 344);
+            this.Controls.Add(this.chk_todos);
+            this.Controls.Add(this.btnConsultar);
+            this.Controls.Add(this.txt_denominacion);
+            this.Controls.Add(this.btnModificar);
+            this.Controls.Add(this.grid_raza);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnNuevo);
+            this.Name = "Peso_x_altura_x_Raza";
+            this.Text = "Peso_x_altura_x_Raza";
+            ((System.ComponentModel.ISupportInitialize)(this.grid_raza)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox chk_todos;
+        private System.Windows.Forms.Button btnConsultar;
+        private System.Windows.Forms.TextBox txt_denominacion;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.DataGridView grid_raza;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nuevo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Consultar;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Modificar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnNuevo;
+    }
+}
---
+++ WindowsFormsApp1/Formularios/FORMULARIO BASE/Formulario_Base_ABM.Designer.cs
@@ -0,0 +1,116 @@
+﻿
+namespace WindowsFormsApp1.Formularios.FORMULARIO_BASE
+{
+    partial class Formulario_Base_ABM
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lbl_titulo = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btn_Alta = new System.Windows.Forms.Button();
+            this.btn_Modificar = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.btn_eliminar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_titulo
+            // 
+            this.lbl_titulo.AutoSize = true;
+            this.lbl_titulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_titulo.Location = new System.Drawing.Point(13, 13);
+            this.lbl_titulo.Name = "lbl_titulo";
+            this.lbl_titulo.Size = new System.Drawing.Size(80, 29);
+            this.lbl_titulo.TabIndex = 0;
+            this.lbl_titulo.Text = "Titulo";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(12, 66);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(823, 116);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Restriccion de calculo";
+            // 
+            // btn_Alta
+            // 
+            this.btn_Alta.Location = new System.Drawing.Point(258, 392);
+            this.btn_Alta.Name = "btn_Alta";
+            this.btn_Alta.Size = new System.Drawing.Size(75, 23);
+            this.btn_Alta.TabIndex = 3;
+            this.btn_Alta.Text = "Agregar";
+            this.toolTip1.SetToolTip(this.btn_Alta, "Permite Ingresar un Alta al Formulario Peso por Altura por Raza");
+            this.btn_Alta.UseVisualStyleBackColor = true;
+            // 
+            // btn_Modificar
+            // 
+            this.btn_Modificar.Location = new System.Drawing.Point(383, 392);
+            this.btn_Modificar.Name = "btn_Modificar";
+            this.btn_Modificar.Size = new System.Drawing.Size(75, 23);
+            this.btn_Modificar.TabIndex = 4;
+            this.btn_Modificar.Text = "Modificar";
+            this.toolTip1.SetToolTip(this.btn_Modificar, "Permite el Ingreso al Formulario de Peso por Altura por Raza");
+            this.btn_Modificar.UseVisualStyleBackColor = true;
+            // 
+            // btn_eliminar
+            // 
+            this.btn_eliminar.Location = new System.Drawing.Point(511, 392);
+            this.btn_eliminar.Name = "btn_eliminar";
+            this.btn_eliminar.Size = new System.Drawing.Size(75, 23);
+            this.btn_eliminar.TabIndex = 4;
+            this.btn_eliminar.Text = "Eliminar";
+            this.toolTip1.SetToolTip(this.btn_eliminar, "Permite Ingresar para Eliminar algun formulario de Peso por Altura por Raza");
+            this.btn_eliminar.UseVisualStyleBackColor = true;
+            // 
+            // Formulario_Base_ABM_Peso_x_Altura_x_Raza
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(847, 439);
+            this.Controls.Add(this.btn_eliminar);
+            this.Controls.Add(this.btn_Modificar);
+            this.Controls.Add(this.btn_Alta);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lbl_titulo);
+            this.Name = "Formulario_Base_ABM_Peso_x_Altura_x_Raza";
+            this.Text = "Formulario_Base";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Label lbl_titulo;
+        protected System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btn_Alta;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Button btn_Modificar;
+        private System.Windows.Forms.Button btn_eliminar;
+    }
+}
