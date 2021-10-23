ff93b8c2278bbd891ace73afa1d135c9a3379e50
tomasparis10 <84345749+tomasparis10@users.noreply.github.com>
2021-10-23 16:58:51 -0300

    agregue una sola cosa 16:58

---
+++ WindowsFormsApp1/Formularios/Reportes/Reporte_peso_altura_x_raza/Reporte_Peso_altura_x_Raza.Designer.cs
@@ -0,0 +1,47 @@
+﻿
+namespace WindowsFormsApp1.Formularios.Reportes.Reporte_peso_altura_x_raza
+{
+    partial class Reporte_Peso_altura_x_Raza
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Reporte_Peso_altura_x_Raza
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Reporte_Peso_altura_x_Raza";
+            this.Text = "Reporte_Peso_altura_x_Raza";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
